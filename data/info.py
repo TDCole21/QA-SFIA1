9b9e34b23b9d1261336619a7c09666fcdf85d829
TDCole21 <thomas.cole@academytrainee.com>
2020-03-06 10:29:30 +0000

    local environments final

--- data/info.py
+++ data/info.py
@@ -1,10 +1,6 @@
 import os
 
 MySQLhost = os.environ['MYSQLHOST']
-#MYSQL_HOST = '34.65.134.225'
 MySQLuser = os.environ['MYSQL_USER']
-#MYSQL_USER = 'root'
 MySQLpassword = os.environ['MYSQL_PASSWORD']
-#MYSQL_PASSWORD = 'Rosliston21'
 MySQLdb = os.environ['MYSQL_DB']
-#MYSQL_DB = 'SFIA1'

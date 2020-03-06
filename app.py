5d446f614686f7cc854187de4c6421dd67b3670f
TDCole21 <thomas.cole@academytrainee.com>
2020-03-06 10:28:13 +0000

    local environments

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 from data import dummydata
 from flask_mysqldb import MySQL
 from data import info
+import os
 
 
 app = Flask(__name__) #__name__ is for best practice
 
-app.config["MYSQL_HOST"] = info.MYSQL_HOST
-app.config["MYSQL_USER"] = info.MYSQL_USER
-app.config["MYSQL_PASSWORD"] = info.MYSQL_PASSWORD
-app.config["MYSQL_DB"] = info.MYSQL_DB
+app.config["MYSQL_HOST"] = info.MySQLhost
+app.config["MYSQL_USER"] = info.MySQLuser
+app.config["MYSQL_PASSWORD"] = info.MySQLpassword
+app.config["MYSQL_DB"] = info.MySQLdb
+
 
 mysql = MySQL(app)
 
@@ -17,9 +19,14 @@ mysql = MySQL(app)
 @app.route('/index')
 @app.route('/home')
 def home():
+    return render_template("index.html", name="Home")
+
+@app.route('/actors')
+def actors():
     cur = mysql.connection.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS testtable(ID INT(3) PRIMARY KEY, name VARCHAR(20));") #cur.execute requires SQL commands
-    cur.execute("SELECT * FROM testtable")
+    #cur.execute("CREATE TABLE IF NOT EXISTS testtable(ID INT(3) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20));") #cur.execute requires SQL commands
+    #cur.execute("INSERT INTO testtable (name) VALUES  (\"John Alley\");")
+    cur.execute("SELECT * FROM Actors")
     mysql.connection.commit()
     rows = cur.fetchall() #built in function to return a tuple, list or dictionary
     cur.close()
@@ -27,17 +34,24 @@ def home():
     info = []
 
     for row in rows:
-        info.append(row) #adding each row from the database into a newly created list, info
-
-    return render_template("index.html", name="Home", page="active", info1=info, posts=dummydata.dummyData)
-
-@app.route('/actors')
-def actors():
-    return render_template("actors.html", name="Actors", page="active", posts=dummydata.dummyData)
+        info.append(row) #adding each row from the database into a newly created list, info    
+    return render_template("actors.html", name="Actors", info1=info, posts=dummydata.dummyData)
 
 @app.route('/films')
 def films():
-    return render_template("films.html", name="Films", page="active", posts=dummydata.dummyData)
+    cur = mysql.connection.cursor()
+    #cur.execute("CREATE TABLE IF NOT EXISTS testtable(ID INT(3) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20));") #cur.execute requires SQL commands
+    #cur.execute("INSERT INTO testtable (name) VALUES  (\"John Alley\");")
+    cur.execute("SELECT * FROM Films")
+    mysql.connection.commit() # This can go after the fetch all
+    rows = cur.fetchall() #built in function to return a tuple, list or dictionary
+    cur.close()
+
+    info = []
+
+    for row in rows:
+        info.append(row) #adding each row from the database into a newly created list, info    
+    return render_template("films.html", name="Films", info1=info)
 
 
 if __name__ == "__main__":

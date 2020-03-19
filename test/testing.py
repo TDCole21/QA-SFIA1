9aae8806518a34ef60e21263c2179a06a5e8c43d
TDCole21 <thomas.cole@academytrainee.com>
2020-03-19 14:55:19 +0000

    Jenkins testing part IV

--- test/testing.py
+++ test/testing.py
@@ -36,6 +36,7 @@ def test_nonpage():                                                         #tes
     assert 404  == r.status
 
 
+
 def test_select():                                               #select test
     with app.app_context():
         cur= mysql.connection.cursor()

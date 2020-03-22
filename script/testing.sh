21c0e8656466816e12812b1a46e951b2ffd22049
TDCole21 <thomas.cole@academytrainee.com>
2020-03-22 16:27:05 +0000

    DB_TESTING

--- script/testing.sh
+++ script/testing.sh
@@ -7,3 +7,8 @@ source /var/lib/jenkins/.bashrc
 coverage run -m pytest ./test/url_testing.py
 
 coverage report
+
+
+coverage run -m pytest ./test/db_testing.py
+
+coverage report
--- test/testing.py
+++ test/testing.py
@@ -1,17 +1,17 @@
 import urllib3
 
-# from flask import Flask
-# from flask_mysqldb import MySQL
-# import os
+from flask import Flask
+from flask_mysqldb import MySQL
+import os
 
-# app=Flask(__name__)
+app=Flask(__name__)
 
-# mysql=MySQL(app)
+mysql=MySQL(app)
 
-# app.config['MYSQL_HOST']=os.environ['MYSQLHOST']
-# app.config['MYSQL_USER']=os.environ['MYSQLUSER']
-# app.config['MYSQL_PASSWORD']=os.environ['MYSQLPASSWORD']
-# app.config['MYSQL_DB']=os.environ['MYSQLDB']
+app.config['MYSQL_HOST']=os.environ['MYSQLHOST']
+app.config['MYSQL_USER']=os.environ['MYSQLUSER']
+app.config['MYSQL_PASSWORD']=os.environ['MYSQLPASSWORD']
+app.config['MYSQL_DB']=os.environ['MYSQLDB']
 
 
 
@@ -32,39 +32,39 @@ def test_films():                                                        #tests 
 
 def test_nonpage():                                                         #tests if page doesn't exist
     http=urllib3.PoolManager()
-    r=http.request('GET', 'http://35.242.157.109:5000/aboutus')
+    r=http.request('GET', 'http://35.242.157.109:5000/')
     assert 404  == r.status
 
-# def test_select():                                               #select test
-#     with app.app_context():
-#         cur= mysql.connection.cursor()
-#         num_of_records=cur.execute('SELECT * FROM Location')    #test made on the table that's not used for CRUD 
-#         mysql.connection.commit()
-#         cur.close()
-#         assert 12 == num_of_records                             #checks if the number of records in the database matches the number of entries expected to have in the table
+def test_select():                                               #select test
+    with app.app_context():
+        cur= mysql.connection.cursor()
+        num_of_records=cur.execute('SELECT * FROM Location')    #test made on the table that's not used for CRUD 
+        mysql.connection.commit()
+        cur.close()
+        assert 12 == num_of_records                             #checks if the number of records in the database matches the number of entries expected to have in the table
 
-# def test_insert():                                              #insert test
-#     with app.app_context():
-#         cur= mysql.connection.cursor()
-#         cur.execute('SELECT * FROM Activity')                   #tests made on the table used for CRUD
-#         num_of_records=cur.fetchall()
-#         cur.execute('INSERT INTO Activity (Name) VALUES ("test")')  #inserts at an autoincrement ID the value 'test'
-#         mysql.connection.commit()
-#         cur.execute('SELECT * FROM Activity')
-#         new_num_records=cur.fetchall()
-#         cur.close()
-#     assert num_of_records[len(num_of_records)-1] != new_num_records[len(new_num_records)-1]
+def test_insert():                                              #insert test
+    with app.app_context():
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT * FROM Activity')                   #tests made on the table used for CRUD
+        num_of_records=cur.fetchall()
+        cur.execute('INSERT INTO Activity (Name) VALUES ("test")')  #inserts at an autoincrement ID the value 'test'
+        mysql.connection.commit()
+        cur.execute('SELECT * FROM Activity')
+        new_num_records=cur.fetchall()
+        cur.close()
+    assert num_of_records[len(num_of_records)-1] != new_num_records[len(new_num_records)-1]
 
 
     
-# def test_delete():                                              #delete test
-#     with app.app_context():
-#         cur= mysql.connection.cursor()
-#         cur.execute('SELECT * FROM Activity')                   #tests made on the table used for CRUD
-#         num_of_records=cur.fetchall()
-#         cur.execute('DELETE FROM Activity WHERE ID= %s', [int(num_of_records[len(num_of_records)-1][0])])  #delete all entries in the table that have the name 'test'
-#         mysql.connection.commit()
-#         cur.execute('SELECT * FROM Activity')
-#         new_num_records=cur.fetchall()
-#         cur.close()
-#     assert num_of_records[len(num_of_records)-1][0] != new_num_records[len(new_num_records)-1][0]
+def test_delete():                                              #delete test
+    with app.app_context():
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT * FROM Activity')                   #tests made on the table used for CRUD
+        num_of_records=cur.fetchall()
+        cur.execute('DELETE FROM Activity WHERE ID= %s', [int(num_of_records[len(num_of_records)-1][0])])  #delete all entries in the table that have the name 'test'
+        mysql.connection.commit()
+        cur.execute('SELECT * FROM Activity')
+        new_num_records=cur.fetchall()
+        cur.close()
+    assert num_of_records[len(num_of_records)-1][0] != new_num_records[len(new_num_records)-1][0]
